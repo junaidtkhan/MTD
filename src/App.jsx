95020a147c6c1665537ed883c41b4e6117a6dc6e
junaidtkhan <junaid3947khan@gmail.com>
2024-09-11 01:32:10 +0300

    deploying on github pages

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { FormPage } from './Components/Form'
+
+function App() {
+  // const [count, setCount] = useState(0)
+  const [formPage, setFormPage] = useState(false)
+
+  return (
+    <div className='app'>
+      {!formPage ? (
+        <div
+        style={{
+          position: "absolute",
+          top: "15%",
+          left: "35%",
+
+        }}
+        >
+          {/* <img src="../public/MTD_Logo.jpg" alt="" /> */}
+          <h2>
+            Welcome to MTD House of Water
+          </h2>
+          <button
+          style={{
+            marginTop: "1rem",
+            backgroundColor: "#fff",
+            color: "#000",
+          }}
+            onClick={() => {
+              setFormPage(true)
+            }}>
+            Click to Start
+          </button>
+        </div>
+      ) :
+        <FormPage setFormPage={setFormPage} />
+      }
+    </div>
+  )
+}
+
+export default App
